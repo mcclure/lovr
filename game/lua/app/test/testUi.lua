0fef09c4c2a5256ccaf7bf56eb1eca41a1d2b13b
mcc <andi.m.mcclure@gmail.com>
2019-08-23 11:43:45 -0400

    Create new andi branch: currently just lovr-ent (hg:d463dd7639a6 git:cd42804bf2ec) plus run.txt plus lovr 0.13b master

---
+++ game/lua/app/test/testUi.lua
@@ -0,0 +1,43 @@
+-- Establish that all the basic features of ui2 work
+
+namespace "standard"
+
+local ui2 = require "ent.ui2"
+
+local TestUi = classNamed("TestUi", ui2.ScreenEnt)
+
+function TestUi:onLoad()
+	ui2.routeMouse()
+	local ents = {
+		ui2.ButtonEnt{label="X", onButton = function(self) -- Test die
+			self.swap:die()
+		end},
+		ui2.UiEnt{label="testUi"},
+		ui2.ButtonEnt{label="one", onButton = function(self)
+				print("ONE!")
+		end},
+		ui2.ButtonEnt{label="two", onButton = function(self)
+				print("TWO!")
+		end},
+		ui2.ButtonEnt{label="three four five", onButton = function(self)
+				print("THREE FOUR FIVE!")
+		end},
+		ui2.ButtonEnt{label="RESET", onButton = function(self) -- Test swap
+			self.swap:swap( TestUi() )
+		end},
+		ui2.ButtonEnt{label=1, onButton = function(self) -- Give this ui some state
+				self.label = self.label + 1
+		end},
+	}
+	for i,v in ipairs{"buttonx", "nonsense", "garb", "garbage", "not", "hing", "nothing", "no", "thing"} do
+		table.insert(ents, ui2.ButtonEnt{label=v})
+	end
+	local slider = ui2.SliderEnt()
+	table.insert(ents, slider)
+	table.insert(ents, ui2.SliderWatcherEnt{watch=slider})
+
+	local layout = ui2.PileLayout{managed=ents, parent=self, pass={swap=self}}
+	layout:layout()
+end
+
+return TestUi
