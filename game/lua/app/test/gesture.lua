8687bce25e37078764b45fb17e0e5f90a2cfdf76
mcc <andi.m.mcclure@gmail.com>
2019-12-31 16:18:51 -0500

    Gesture works

--- game/lua/app/test/gesture.lua
+++ game/lua/app/test/gesture.lua
@@ -5,17 +5,27 @@ local HandCubes = require "ent.debug.handCubes"
 local Floor = require "ent.debug.floor"
 local GestureTest = classNamed("GestureTest", HandCubes)
 
-local lightgray = {0.85,0.85,0.85}
 local cubeTickRate=0.075
-local fingerHighlight = true
+local fingerHighlight = false
+
+local gunCurledUnder = .001
+local gunThumbCockUnder = .004
+local gunThumbFireOver = .005
+
+local crayon = { magenta = {1,0,1}, pink = {1,0.3647,0.7176},
+	cyan = {0,1,1}, purple = {0.5,0,1}, lime = {0,1,0.5}, lavender= {0.5, 0.5, 1}, yellow = {1, 1, 0.5},
+	lightcyan = {0.9,1,1}, red={1,0,0}
+}
+
+local deselected = fingerHighlight and {0.85,0.85,0.85} or crayon.lightcyan
 
 function GestureTest:onLoad()
 	lovr.graphics.setBackgroundColor(0.95,0.975,1)
-	self.color = lightgray
+	self.color = deselected
 	HandCubes.onLoad(self)
 	Floor():insert(self)
 
-	self.handData = {{curl={}},{curl={}}}
+	self.handData = {{curl={}, gunGesture={}},{curl={}, gunGesture={}}}
 
 	-- Positioning for infoboxes
 	local baseTransform = Loc(vec3(0,0,2)):precompose(Loc(nil, quat.from_angle_axis(math.pi, 0,1,0)))
@@ -28,6 +38,7 @@ end
 function GestureTest:handColors(handI, handName, points)
 	-- Calculate gesture proxies. Do this here in case we want to establish a color based on gesture
 	local hand = self.handData[handI]
+	local gunGesture = true
 
 	if not hand.name then hand.name = handName end
 
@@ -42,6 +53,12 @@ function GestureTest:handColors(handI, handName, points)
 			curl = curl + (at_0-at_1):dot(at_1-at_2)
 		end
 		hand.curl[i] = curl
+		if i>1 then
+			local fingerCurled = curl < gunCurledUnder
+			local fingerGunGesture = (i==2 and not fingerCurled) or (i>2 and fingerCurled)
+			if not fingerGunGesture then gunGesture = false end
+			hand.gunGesture[i] = fingerGunGesture
+		end
 	end
 
 	-- Thumb/index alignment
@@ -56,8 +73,50 @@ function GestureTest:handColors(handI, handName, points)
 		gunClick = gunClick + (at_0-at_1):dot(gunPoint)
 	end
 	hand.gunClick = gunClick
+
+	local canFire = gunClick > gunThumbFireOver
+	local canCock = gunClick < gunThumbCockUnder
+
+	local wasCocked = hand.cocked
+	local wasFire = hand.fire
+	hand.fire = (wasCocked or wasFire) and canFire
+	hand.trigger = wasCocked and not wasFire and hand.fire
+	hand.cocked = not hand.trigger and ( (gunGesture and wasCocked) or canCock )
+	if handI==2 and wasCocked and not hand.cocked then
+		print("Lost it...", hand.trigger, "_2", gunGesture, "_3", wasCocked, "_4", canCock, "_5", not hand.trigger, "_6", (gunGesture and wasCocked), "_7", ( (gunGesture and wasCocked) or canCock ))
+	end
+	hand.cockState = canFire and 3 or (canCock and 2 or 1)
+
+	if hand.trigger then
+		local indexRoot = points[index[1]].at
+		local indexTip = points[index[#index]].at
+		if handI==2 then print("BANG!", indexTip, indexTip + (indexTip-indexRoot)*10, wasCocked and not hand.cocked) end
+		self.cubes:add{at=indexTip, lineTo=indexTip + (indexTip-indexRoot)*10, lineColor=crayon.red, noCube=true}
+	end
+	if hand.fire then
+		return crayon.red
+	elseif hand.cocked then
+		return crayon.pink
+	end
 end
 
+local font
+function printColors(spec, baseColor)
+	font = font or lovr.graphics.getFont()
+	local top = #spec * font:getLineHeight() / 2
+	for i,v in ipairs(spec) do
+		local text, color
+		if type(v) == "table" then text,color = unpack(v) else text = v end
+		if text then
+			color = color or baseColor
+			lovr.graphics.setColor(unpack(color))
+			lovr.graphics.print(text, 0, top - i + 1, 0, nil,nil,nil,nil,nil,nil, 'center', 'bottom')
+		end
+	end
+end
+
+local graytext = {0.5, 0.5, 0.5}
+local cockColors = {false, crayon.pink, crayon.red}
 function GestureTest:onDraw()
 	for i,hand in ipairs(self.handData) do
 		self.screenTransforms[i]:push()
@@ -65,12 +124,15 @@ function GestureTest:onDraw()
 		lovr.graphics.scale(0.25)
 
 		-- Name
-		local str = (hand.name or "(nil)") .. "\n"
+		local t = {}
+		table.insert(t, hand.name or "(nil)")
+		table.insert(t, false)
 		for i2,curl in ipairs(hand.curl) do
-			str = string.format("%s\n%d: %.07f", str, i2, curl)
+			table.insert(t, {string.format("%d: %.07f", i2, curl), hand.gunGesture[i2] and (i2==2 and crayon.red or crayon.pink)})
 		end
-		str = string.format("%s\n\nFingergun: %.07f", str, hand.gunClick)
-		lovr.graphics.print(str)
+		table.insert(t, false)
+		table.insert(t, {string.format("Fingergun: %.07f", hand.gunClick), cockColors[hand.cockState]})
+		printColors(t, graytext)
 		
 		lovr.graphics.pop()
 	end
@@ -80,7 +142,7 @@ end
 function GestureTest:pointColors(handI, hand, points, pointI, point)
 	if fingerHighlight then -- Mark joints used for gesture calc
 		if not self.whichFingerColor then
-			self.whichFingerColor = {{1,0,1}, {0,1,1}, {0.5,0,1}, {0,1,0.5}, {1,0.3647,0.7176}} --Thumb=magenta, Index=cyan, Middle=purple, Ring=lime, Pinky=pink
+			self.whichFingerColor = {crayon.cyan, crayon.purple, crayon.lime, crayon.lavender, crayon.yellow}
 			self.whichFinger = {}
 			for i,joints in ipairs(HandCubes.skeletonFingers) do
 				for i2, joint in ipairs(joints) do
@@ -92,7 +154,7 @@ function GestureTest:pointColors(handI, hand, points, pointI, point)
 		local color = which and self.whichFingerColor[which]
 		if color then
 			local from = self.whichFinger[self.skeletonSkeleton[pointI]]
-			return color, from and color or lightgray
+			return color, from and color or deselected
 		end
 	end
 end
