381ac71da7b9e9b2acae64d28a1bc4d97f2c30f3
mcc <andi.m.mcclure@gmail.com>
2020-01-03 15:58:28 -0500

    Prism and can model

--- game/lua/app/test/gesture.lua
+++ game/lua/app/test/gesture.lua
@@ -14,7 +14,7 @@ local gunThumbFireOver = .005
 
 local crayon = { magenta = {1,0,1}, pink = {1,0.3647,0.7176},
 	cyan = {0,1,1}, purple = {0.5,0,1}, lime = {0,1,0.5}, lavender= {0.5, 0.5, 1}, yellow = {1, 1, 0.5},
-	lightcyan = {0.9,1,1}, red={1,0,0}
+	lightcyan = {0.9,1,1}, red={1,0,0}, mustard={0.5, 0.5, 0}
 }
 
 local deselected = fingerHighlight and {0.85,0.85,0.85} or crayon.lightcyan
@@ -33,11 +33,10 @@ function GestureTest:onLoad()
 	} })
 	self.shader:send("lovrLightDirection", {-1,-1,-1})
 	self.shader:send("lovrLightColor", {1,1,1,2})
-	self.canBaseAt = vec3(0,2,-2)
-	self.postRad = 0.1
+	self.canBaseAt = vec3(0,1,-2)
+	self.postRad = 0.2
 	self.canScale = 1/1000
-	self.postOrient = {math.pi/2, 1,0,0}
-	self.postOrientDraw = self.postOrient
+	self.canOrient = {math.pi/2, 1,0,0}
 	self.canModel = lovr.graphics.newModel("gestureDemo/can_soup/scene.gltf")
 	self.cowbellSound = lovr.audio.newSource("gestureDemo/cowbell.ogg", "static")
 	self.bangSound = lovr.audio.newSource("gestureDemo/whitenoise.ogg", "static")
@@ -45,10 +44,8 @@ function GestureTest:onLoad()
 	self.world:setLinearDamping(.01)
 	self.world:setAngularDamping(.005)
 	self.world:newBoxCollider(0, -0.5, 0, 50, 1, 50):setKinematic(true) -- Ground??
-	--local ground = self.world:newCylinderCollider(0, 0, 0, floor.floorSize/2, .05)
-	--ground:setOrientation(unpack(self.postOrient)) ground:setKinematic(true)
-	local post = self.world:newCylinderCollider(self.canBaseAt.x, self.canBaseAt.y/2, self.canBaseAt.z, self.postRad, self.canBaseAt.y)
-	post:setOrientation(unpack(self.postOrient))   post:setKinematic(true)
+	local post = self.world:newBoxCollider(self.canBaseAt.x, self.canBaseAt.y/2, self.canBaseAt.z, self.postRad, self.canBaseAt.y, self.postRad)
+	post:setKinematic(true)
 	self.debugPost = post
 	-- The can's collider needs to be based on the metrics of the model, but the model is actually very messy
 	local canBox = {}
@@ -56,10 +53,11 @@ function GestureTest:onLoad()
 	for k,v in pairs(canBox) do canBox[k] = v * self.canScale end
 	print(canBox.minx, canBox.maxx, canBox.miny, canBox.maxy, canBox.minz, canBox.maxz)
 	local canHeight, canRadius = canBox.maxy-canBox.miny, (canBox.maxx-canBox.minx)/2
+	self.canCenter = -(vec3(canBox.maxx, canBox.maxy, canBox.maxz) + vec3(canBox.minx, canBox.miny, canBox.minz))/2 / self.canScale
+	print(self.canCenter:unpack())
 	self.canDebug = {canHeight, canRadius}
-	self.can = self.world:newCylinderCollider(self.canBaseAt.x, self.canBaseAt.y + canHeight*2, self.canBaseAt.z, canRadius, canHeight) -- Can
-	self.can:setOrientation(unpack(self.postOrient))
-	self.can:setAngularVelocity(0,0,-4)
+	self.can = self.world:newCylinderCollider(self.canBaseAt.x, self.canBaseAt.y + canHeight, self.canBaseAt.z, canRadius, canHeight) -- Can
+	self.can:setOrientation(unpack(self.canOrient))
 
 	-- Positioning for infoboxes
 	local baseTransform = Loc(vec3(0,0,2)):precompose(Loc(nil, quat.from_angle_axis(math.pi, 0,1,0)))
@@ -70,7 +68,9 @@ function GestureTest:onLoad()
 end
 
 function GestureTest:onUpdate(dt)
-	self.world:update(dt)
+	for i=1,3 do
+		self.world:update(dt/3)
+	end
 end
 
 function GestureTest:handColors(handI, handName, points)
@@ -126,7 +126,7 @@ function GestureTest:handColors(handI, handName, points)
 		local indexRoot = points[index[1]].at
 		local indexTip = points[index[#index]].at
 		self.bangSound:play()
-		self.cubes:add{at=indexTip, lineTo=indexTip + (indexTip-indexRoot)*10, lineColor=crayon.red, noCube=true}
+		self.cubes:add{at=indexTip, lineTo=indexTip + (indexTip-indexRoot)*30, lineColor=crayon.red, noCube=true}
 	end
 	if hand.fire then
 		return crayon.red
@@ -176,20 +176,24 @@ function GestureTest:onDraw()
 	end
 
 	lovr.graphics.setShader(self.shader)
-	lovr.graphics.setColor(1,0,0)
+	lovr.graphics.setColor(unpack(crayon.mustard))
 
 	do
 		local x,y,z = self.debugPost:getPosition()
 		local a,ax,ay,az = self.debugPost:getOrientation()
-		lovr.graphics.cylinder(x, y, z, self.canBaseAt.z, a,ax,ay,az, self.postRad,self.postRad)
+		lovr.graphics.box('fill', x, y, z, self.postRad, self.canBaseAt.y, self.postRad, a,ax,ay,az)
 	end
-	--lovr.graphics.cylinder(self.canBaseAt.x, self.canBaseAt.y, self.canBaseAt.z/2, self.canBaseAt.z, self.postOrient[1],self.postOrient[2],self.postOrient[3],self.postOrient[4], self.postRad,self.postRad)
 
+	lovr.graphics.setColor(1,1,1)
 	do
-		local x,y,z = self.can:getPosition()
-		--self.canModel:draw(x,y,z, self.canScale, self.can:getOrientation())
-		local a,ax,ay,az = self.can:getOrientation()
-		lovr.graphics.cylinder(x,y,z, self.canDebug[1], a,ax,ay,az, self.canDebug[2],self.canDebug[2])
+		Loc(vec3(self.can:getPosition()), quat.from_angle_axis(self.can:getOrientation())):push()
+		lovr.graphics.scale(self.canScale)
+		lovr.graphics.rotate(-math.pi/2,1,0,0)
+		lovr.graphics.translate(self.canCenter:unpack())
+		self.canModel:draw()
+		--local a,ax,ay,az = self.can:getOrientation()
+		--lovr.graphics.cylinder(x,y,z, self.canDebug[1], a,ax,ay,az, self.canDebug[2],self.canDebug[2])
+		lovr.graphics.pop()
 	end
 end
 
