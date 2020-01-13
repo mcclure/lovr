75f49deb1269723b427aa044b7d11b7c7b64d5db
mcc <andi.m.mcclure@gmail.com>
2020-01-13 01:21:38 -0500

    Horrible supersquare

--- game/lua/midi/audio/gen/saw.lua
+++ game/lua/midi/audio/gen/saw.lua
@@ -13,22 +13,56 @@ end
 
 local USHRT_MAX = 0xffff
 local SHRT_MIN = -32767
+local UN_SHRT_MIN = -SHRT_MIN
+
+local function conv(x)
+	return math.max(-1, math.min(1, x*100))*SHRT_MIN
+end
+
 local function saw(x)
-	return x % USHRT_MAX + SHRT_MIN
+	return (x*2) % 2 - 1
+end
+
+local SawSource = classNamed("SawSource")
+
+function SawSource:pre(follow)
+	if not self.ticks then self.ticks = 0 end
+	if follow then 
+		if follow.f ~= self.lastf then
+			self.incr = self.incr and (self.incr+1) or 1
+			self.lastf = follow.f
+		end
+		self.f = follow.f + (self.incr/(44100*8))
+	elseif not self.f or self.ticks - self.lastf > 100 then
+		self.f = math.random(32,512)/44100
+		self.lastf = self.ticks
+	end
+end
+
+function SawSource:get(i)
+	return saw(self.ticks + i*self.f)
+end
+
+function SawSource:post(lasti)
+	self.ticks = self.ticks + lasti*self.f
 end
 
 function Generator:audio(blob)
-	local f = self.f -- Something kinda interesting
-	if not self.f or self.ticks - self.lastf > 500000 then self.f = math.random(64,512) self.lastf = self.ticks f = self.f end
+	if not self.gens then self.gens = {SawSource(), SawSource(), SawSource()} end
 
 	local bytes = blob:getSize()
 	local samples = bytes/2
 	local _ptr = blob:getPointer()
 	local ptr = ffi.cast(shortptr, _ptr)
-	for i=0,(samples-1) do
-		ptr[i] = saw(self.ticks + i*f)
+
+	for i,v in ipairs(self.gens) do v:pre(self.gens[i-1]) end
+
+	for i=1,samples do
+		ptr[i-1] = conv( self.gens[1]:get(i) + self.gens[2]:get(i) + self.gens[3]:get(i) )
 	end
-	self.ticks = self.ticks + samples*f
+
+	for i,v in ipairs(self.gens) do v:post(samples) end
+
 	return blob
 end
 
